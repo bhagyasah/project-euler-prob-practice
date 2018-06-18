67b9e707e8a7321551f8b6b0cd5f4867dbdd19c9
Bhagya Sah <bhagyasah4u@gmail.com>
2018-06-18 17:30:26 +0545

    space reduce

--- src/10-20/q12.js
+++ src/10-20/q12.js
@@ -1,11 +1,11 @@
 function heighlyDivisibleNumSum(n) {
 	let sum = 0;
 	let count	=	0;
-	for	(let i = 1; count !== n; i	+= 1)	{
+	for (let i = 1; count !== n; i	+= 1)	{
 		sum += i;
 		const sqrtSum = Math.sqrt(sum);
 		// console.log('sum is',sum,sqrtSum);
-		for	(let j = 1; j <= sqrtSum; j += 1) {
+		for (let j = 1; j <= sqrtSum; j += 1) {
 			if	(sum % j === 0)	{
 				count	+=	1;
 			}
@@ -15,7 +15,7 @@ function heighlyDivisibleNumSum(n) {
 			count -= 1;
 		}
 		//	console.log("number of divisor",count);
-		if	(count >= n) {
+		if (count >= n) {
 			break;
 		}	else {
 			count	=	0;

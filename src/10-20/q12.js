6d047fc7f193e500eb20ea92626108f752496f78
Bhagya Sah <bhagyasah4u@gmail.com>
2018-06-19 08:20:59 +0545

    tab adusted

--- src/10-20/q12.js
+++ src/10-20/q12.js
@@ -1,27 +1,26 @@
 function heighlyDivisibleNumSum(n) {
-	let sum = 0;
-	let count	=	0;
-	for (let i = 1; count !== n; i	+= 1)	{
-		sum += i;
-		const sqrtSum = Math.sqrt(sum);
-		// console.log('sum is',sum,sqrtSum);
-		for (let j = 1; j <= sqrtSum; j += 1) {
-			if	(sum % j === 0)	{
-				count	+=	1;
-			}
-		}
-		count *= 2;
-		if (Number.isInteger(Math.sqrt(sum))) {
-			count -= 1;
-		}
-		//	console.log("number of divisor",count);
-		if (count >= n) {
-			break;
-		} else {
-			count	=	0;
-		}
-	}
-	return sum;
+  let sum = 0;
+  let count	=	0;
+  for (let i = 1; count !== n; i	+= 1)	{
+    sum += i;
+    const sqrtSum = Math.sqrt(sum);
+    //  console.log('sum is',sum,sqrtSum);
+    for (let j = 1; j <= sqrtSum; j += 1) {
+      if	(sum % j === 0)	{
+        count	+=	1;
+      }
+    }
+    count *= 2;
+    if (Number.isInteger(Math.sqrt(sum))) {
+      count -= 1;
+    }
+    //  console.log("number of divisor",count);
+    if (count >= n) {
+      break;
+    } else {
+      count	=	0;
+    }
+  }
+  return sum;
 }
-
 console.log("result is",	heighlyDivisibleNumSum(500));

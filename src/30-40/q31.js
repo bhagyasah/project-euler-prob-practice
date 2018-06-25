a81242d8968eb19cbbf3c87ecacab3ebf5d5aaeb
Bhagya Sah <bhagyasah4u@gmail.com>
2018-06-25 13:47:45 +0545

    q31 solved

---
+++ src/30-40/q31.js
@@ -0,0 +1,36 @@
+
+const resultArray = [];
+let tempArray = [];
+function createTable(c, t) {
+  for (let i = 0; i < c; i += 1) {
+    for (let j = 0; j <= t; j += 1) {
+      if (i === 0 || (j === 0 && i !== 0)) {
+        tempArray[j] = 1;
+      } else {
+        tempArray[j] = 0;
+      }
+    }
+    resultArray.push(tempArray);
+    tempArray = [];
+  }
+}
+
+function main(target) {
+  let coins = [1, 2, 5, 10, 20, 50, 100, 200];
+  createTable(coins.length, target);
+  for (let i = 1; i < resultArray.length; i += 1) {
+    for (let j = 1; j <= target; j += 1) {
+      //console.log(coins[i]);
+      if (coins[i] > j) {
+        resultArray[i][j] = resultArray[i - 1][j];
+      } else {
+        resultArray[i][j] = resultArray[i - 1][j] + resultArray[i][j - coins[i]];
+      }
+    }
+  }
+  console.log(resultArray);
+  console.log(resultArray[coins.length - 1][target]);
+}
+main(200);
+
+
